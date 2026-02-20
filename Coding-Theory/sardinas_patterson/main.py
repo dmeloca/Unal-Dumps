b60d2b3f5fe0e7855e568ca7db8348bef3aa099f
ch1p <REDACTED>
2026-02-20 14:50:36 -0500

    fix(sardinas-patterson): fix forever loop when sets are cyclic

--- Coding-Theory/sardinas_patterson/main.py
+++ Coding-Theory/sardinas_patterson/main.py
@@ -1,28 +1,48 @@
-def solve_equations(alphabet: set[str], code: set[str], c_prev: set[str]) -> set[str]:
+TEST: list[set[str]] = [{'0', '01', '11'}, 
+                         {'02', '12', '120', '20', '21'},
+                         {'02', '12', '120', '21'}]
+
+def solve_equations(code: set[str], c_prev: set[str]) -> set[str]:
     c_next: set[str] = set()
     for x in code:
         for y in c_prev:
             if y.startswith(x):
                 c_next.add(y[len(x):])
-            if x.startswith(y):
+            elif x.startswith(y):
                 c_next.add(x[len(y):])
     c_next.discard("")            
     return c_next
 
+def is_uniquely_decodable(code: set[str]):
+    counter: int = 0
+
+    current: set[str] = set(code)
+    seen: set[frozenset[str]] = set()
+
+    while current:
+        print(f"c_{counter} = {current}")
+
+        frozen = frozenset(current)
+        if frozen in seen:
+            return True
+        seen.add(frozen)
+
+        next_set = solve_equations(code, current)
+
+        if next_set & code:
+            return False
+
+        current = next_set
+        counter += 1
+    return True
+    
 
 def main() -> None:
-    alphabet: set[str] = {'0', '1'}
-    code: set[str] = {'0', '01', '011'}
-    counter: int = 0
-    c_i: set[str] = set(code)
-
-    while True:
-        print(f"c_{counter} = {c_i}")
-        c_next = solve_equations(alphabet, code, c_i)
-        if c_next == c_i:
-            break
-        c_i = c_next
-        counter += 1
+    code = TEST[2]
+    if is_uniquely_decodable(code):
+        print(f"[!] Uniquely decodable.")
+    else:
+        print("[!] Not uniquely decodable.")
 
 if __name__ == "__main__":
     main()
