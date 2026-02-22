aba56594043825c64699f8b190d3dc6bb99a09fc
dmeloca <melocarrillodaniel@gmail.com>
2026-02-21 22:35:08 -0500

    feat(tree code view): prune tree for a given code

--- Coding-Theory/tree_code_view/main.py
+++ Coding-Theory/tree_code_view/main.py
@@ -16,13 +16,25 @@ class Tree:
                     edges.add(frozenset({prefix, node}))
             current = next_level
         return nodes, edges
+    
+    def prune_tree(self, code: set[str]):
+        nodes: set[str] = set(self.nodes)
+        edges: set[frozenset[str]] = set(self.edges)
+        for code_word in code:
+            for node in list(nodes):
+                if node!= code_word and node.startswith(code_word):
+                    nodes.discard(node)
+        for edge in list(edges):
+            if not(edge.issubset(nodes)):
+                edges.discard(edge)
+        return nodes, edges
 
 def main() -> None:
     alphabet: set[str] = {'0', '1'}
     max_level: int = 3
+    code: set[str] = {'0', '10', '11'}
     tree: Tree = Tree(alphabet, max_level)
-    print(tree.nodes)
-    print(tree.edges)
+    nodes, edges = tree.prune_tree(code)
 
 if __name__ == "__main__":
     main()
