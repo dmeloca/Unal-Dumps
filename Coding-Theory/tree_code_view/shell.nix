375a2e4e8ed5dc53879066a4122ccf101018d1e9
dmeloca <melocarrillodaniel@gmail.com>
2026-02-21 23:21:55 -0500

    nix(tree code view): add a shell hook for automating sourcing and installing requirements

--- Coding-Theory/tree_code_view/shell.nix
+++ Coding-Theory/tree_code_view/shell.nix
@@ -4,4 +4,13 @@ pkgs.mkShell {
     pkgs.python315
     pkgs.graphviz
   ];
+  shellHook = ''
+    if [ ! -d .venv ]; then
+      python -m venv .venv
+      source .venv/bin/activate
+      pip install -r requirements.txt
+    else
+      source .venv/bin/activate
+    fi
+  '';
 }
