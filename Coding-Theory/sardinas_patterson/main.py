ebb1aa4648b367da9bcebf0426536a457c5e5548
dmeloca <melocarrillodaniel@gmail.com>
2026-05-01 21:55:21 -0500

    fix(sardinas): fix function return type

--- Coding-Theory/sardinas_patterson/main.py
+++ Coding-Theory/sardinas_patterson/main.py
@@ -1,19 +1,22 @@
-TEST: list[set[str]] = [{'0', '01', '11'}, 
-                         {'02', '12', '120', '20', '21'},
-                         {'02', '12', '120', '21'}]
+TEST: list[set[str]] = [
+    {"3323", "3223", "1332", "1333", "13", "233", "232"},
+    {"021", "22021", "212", "12121", "12100", "2122"},
+]
+
 
 def solve_equations(code: set[str], c_prev: set[str]) -> set[str]:
     c_next: set[str] = set()
     for x in code:
         for y in c_prev:
             if y.startswith(x):
-                c_next.add(y[len(x):])
+                c_next.add(y[len(x) :])
             elif x.startswith(y):
-                c_next.add(x[len(y):])
-    c_next.discard("")            
+                c_next.add(x[len(y) :])
+    c_next.discard("")
     return c_next
 
-def is_uniquely_decodable(code: set[str]) -> Bool:
+
+def is_uniquely_decodable(code: set[str]) -> bool:
     counter: int = 0
 
     current: set[str] = set(code)
@@ -35,14 +38,15 @@ def is_uniquely_decodable(code: set[str]) -> Bool:
         current = next_set
         counter += 1
     return True
-    
+
 
 def main() -> None:
-    code = TEST[2]
+    code = TEST[1]
     if is_uniquely_decodable(code):
-        print(f"[!] Uniquely decodable.")
+        print("[!] Uniquely decodable.")
     else:
         print("[!] Not uniquely decodable.")
+
 
 if __name__ == "__main__":
     main()
